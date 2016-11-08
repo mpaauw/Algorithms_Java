6a7131c95c8e77f5ecf3c4a0c55679e004cf6321
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-07 16:05:59 -0800

    Pushing initial commit with existing codebase.

---
+++ src/Sort/EfficientSort/HeapSort.java
@@ -0,0 +1,18 @@
+package Sort.EfficientSort;
+
+public class HeapSort {
+    /*
+    * SUMMARY:  Takes an unsorted array and performs a Heap Sort on it.
+    * */
+    public static void heapSort(int[] inputArray){
+
+    }
+
+    private static void heapify(int[] inputArray){
+
+    }
+
+    private static void siftDown(int[] inputArray, int start, int end){
+
+    }
+}
