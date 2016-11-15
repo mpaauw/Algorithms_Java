9949069d3ad83d6a76d8bcca132af82d618d63b8
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-15 11:21:56 -0800

    Added a unit test to the QuickSort testing file.

--- src/Sort/EfficientSort/QuickSort/QuickSort_Test.java
+++ src/Sort/EfficientSort/QuickSort/QuickSort_Test.java
@@ -1,7 +1,19 @@
 package Sort.EfficientSort.QuickSort;
 
+import Sort.AlgorithmTestTools;
+import com.sun.xml.internal.bind.v2.model.annotation.Quick;
 import org.junit.Test;
 
 public class QuickSort_Test {
+
+    private QuickSort _quickSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+
+    @Test
+    public void testQuickSort() throws Exception{
+        _quickSort = new QuickSort();
+        _quickSort.quickSort(_testTools._unsortedSampleIntegerArray);
+        _testTools.compare(_testTools._unsortedSampleIntegerArray, _testTools._sortedSampleIntegerArray);
+    }
 
 }
