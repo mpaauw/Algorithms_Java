4b40467020d13cbbb68a224e6bc50ebab280720a
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 13:36:42 -0800

    Added an algorithm for the combSort() method within the CombSort class.

--- src/Sort/ComparisonSort/CombSort/CombSort.java
+++ src/Sort/ComparisonSort/CombSort/CombSort.java
@@ -1,4 +1,29 @@
 package Sort.ComparisonSort.CombSort;
 
 public class CombSort {
+
+    public void combSort(int[] inputArray){
+        double gap = inputArray.length;
+        double shrinkFactor = 1.3;
+        boolean sorted = false;
+        while(!sorted){
+            gap = Math.floor(gap / shrinkFactor);
+            if(gap > 1){
+                sorted = false;
+            }
+            else{
+                gap = 1;
+                sorted = true;
+            }
+            int i = 0;
+            while(i + gap < inputArray.length){
+                if(inputArray[i] > inputArray[i + (int)gap]){
+                    int temp = inputArray[i];
+                    inputArray[i] = inputArray[i + (int)gap];
+                    inputArray[i + (int)gap] = inputArray[i];
+                }
+                i++;
+            }
+        }
+    }
 }
