2739eb82e3d6725db6679170767ebeb0926b200c
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 14:14:30 -0800

    Added a unit test for the CountingSort() method to the CountingSort test file.

--- src/Sort/ComparisonSort/DistributionSort/CountingSort_Test.java
+++ src/Sort/ComparisonSort/DistributionSort/CountingSort_Test.java
@@ -8,4 +8,10 @@ public class CountingSort_Test {
     private CountingSort _countingSort;
     private AlgorithmTestTools _testTools = new AlgorithmTestTools();
 
+    @Test
+    public void testCountingSort() throws Exception{
+        _countingSort = new CountingSort();
+        _countingSort.countingSort(_testTools._unsortedSampleIntegerArray);
+        _testTools.compare(_testTools._unsortedSampleIntegerArray, _testTools._sortedSampleIntegerArray);
+    }
 }
