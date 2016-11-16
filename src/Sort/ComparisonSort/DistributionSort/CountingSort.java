ba08e45284d151aeb03635bd2ca75e720293166a
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 14:10:15 -0800

    Added an algorithm to the countingSort() method within the CountingSort class.

--- src/Sort/ComparisonSort/DistributionSort/CountingSort.java
+++ src/Sort/ComparisonSort/DistributionSort/CountingSort.java
@@ -1,4 +1,21 @@
 package Sort.ComparisonSort.DistributionSort;
 
 public class CountingSort {
+
+    public void countingSort(int[] inputArray){
+        int[] indexArray = new int[inputArray.length + 1], returnArray = new int[inputArray.length];
+        for(Integer item : inputArray){ // add unsorted items to index array
+            indexArray[item]++;
+        }
+        for(int i = 1; i < indexArray.length; i++){ // count totals in index array
+            int total = indexArray[i] + indexArray[i - 1];
+            indexArray[i] = total;
+        }
+        for(int i = inputArray.length - 1; i >= 0; i--){
+            int sortValue = inputArray[i];
+            int indexValue = indexArray[sortValue] - 1;
+            returnArray[indexValue] = sortValue;
+        }
+        inputArray = returnArray;
+    }
 }
