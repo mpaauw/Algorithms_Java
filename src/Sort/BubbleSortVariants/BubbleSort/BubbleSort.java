8807fbc1d43d7ab2d9e6acdad863edbf07e89d57
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 09:07:06 -0800

    Added code to the bubbleSort() method within the BubbleSort class.

--- src/Sort/BubbleSortVariants/BubbleSort/BubbleSort.java
+++ src/Sort/BubbleSortVariants/BubbleSort/BubbleSort.java
@@ -1,4 +1,19 @@
 package Sort.BubbleSortVariants.BubbleSort;
 
 public class BubbleSort {
+    public void bubbleSort(int[] inputArray){
+        int n = inputArray.length;
+        while(n != 0){
+            int swapIter = 0;
+            for(int i = 0; i < n - 1; i++){
+                if(inputArray[i - 1] > inputArray[i]){
+                    int temp = inputArray[i - 1];
+                    inputArray[i - 1] = inputArray[i];
+                    inputArray[i] = temp;
+                    swapIter = i;
+                }
+            }
+            n = swapIter;
+        }
+    }
 }
