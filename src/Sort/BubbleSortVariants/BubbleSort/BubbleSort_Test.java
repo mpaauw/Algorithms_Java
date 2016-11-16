83316fcb13188d304a3048081aa524d3302938c7
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 09:15:34 -0800

    Added a test file for the Bubble Sort class; added boilerplate code to the BubbleSort test file.

---
+++ src/Sort/BubbleSortVariants/BubbleSort/BubbleSort_Test.java
@@ -0,0 +1,9 @@
+package Sort.BubbleSortVariants.BubbleSort;
+
+import Sort.AlgorithmTestTools;
+import org.junit.Test;
+
+public class BubbleSort_Test {
+    private BubbleSort _bubbleSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+}
