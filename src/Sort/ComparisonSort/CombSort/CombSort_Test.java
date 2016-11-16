f02703dce295e66662beea6cbdaaa581f9ac71af
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 13:37:43 -0800

    Added a test file to the CombSort package.

---
+++ src/Sort/ComparisonSort/CombSort/CombSort_Test.java
@@ -0,0 +1,11 @@
+package Sort.ComparisonSort.CombSort;
+
+import Sort.AlgorithmTestTools;
+import org.junit.Test;
+
+public class CombSort_Test {
+
+    private CombSort _combSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+
+}
