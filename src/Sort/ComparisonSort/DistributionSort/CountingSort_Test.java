c70f9cc153a9beb2223a9653c51e40303f1b985d
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 14:11:34 -0800

    Added a test file for the CountingSort class; added boilerplate code to the CountingSort test file.

---
+++ src/Sort/ComparisonSort/DistributionSort/CountingSort_Test.java
@@ -0,0 +1,11 @@
+package Sort.ComparisonSort.DistributionSort;
+
+import Sort.AlgorithmTestTools;
+import org.junit.Test;
+
+public class CountingSort_Test {
+
+    private CountingSort _countingSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+
+}
