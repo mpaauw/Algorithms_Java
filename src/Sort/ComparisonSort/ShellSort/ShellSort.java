46f3f6d720512aabcb8dcbb5a4adbe0fbfd6fc5f
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 13:18:12 -0800

    Fixed various bugs inside of the shellSort() method within the ShellSort class; created a unit test for the shellSort() method within the ShellSort test file, tested successfully.

--- src/Sort/ComparisonSort/ShellSort/ShellSort.java
+++ src/Sort/ComparisonSort/ShellSort/ShellSort.java
@@ -1,29 +1,25 @@
 package Sort.ComparisonSort.ShellSort;
 
+import java.util.ArrayList;
+
 public class ShellSort {
 
+    private static int[] _array;
+    private static ArrayList<Integer> _gaps = new ArrayList<Integer>();
+
     /*
-    * SUMMARY:  Takes an unsorted array and performs a Shell Sort on it.S
+    * SUMMARY:  Takes an unsorted array and performs a Shell Sort on it.
     *           Efficiency: O(n(log(n))^2)
     * */
     public void shellSort(int[] inputArray){
-        int n = inputArray.length;
-        boolean swapped = false;
-        int gap = n;
-        while(!swapped){
-            swapped = false;
-            gap = gap / 2;
-            int end = inputArray[gap];
-            while(end != inputArray.length){
-                int left = end - gap;
-                int right = end;
-                if(inputArray[left] > inputArray[right]){
-                    int temp = inputArray[left];
-                    inputArray[left] = inputArray[right];
-                    inputArray[right] = temp;
-                    swapped = true;
+        int j;
+        for(int gap = inputArray.length; gap > 0; gap /= 2){
+            for(int i = gap; i < inputArray.length; i++){
+                int temp = inputArray[i];
+                for(j = i; j >= gap && temp < inputArray[j - gap]; j -= gap){
+                    inputArray[j] = inputArray[j - gap];
                 }
-                end++;
+                inputArray[j] = temp;
             }
         }
     }
--- src/Sort/ComparisonSort/ShellSort/ShellSort_Test.java
+++ src/Sort/ComparisonSort/ShellSort/ShellSort_Test.java
@@ -8,4 +8,10 @@ public class ShellSort_Test {
     private ShellSort _shellSort;
     private AlgorithmTestTools _testTools = new AlgorithmTestTools();
 
+    @Test
+    public void testShellSort() throws Exception{
+        _shellSort = new ShellSort();
+        _shellSort.shellSort(_testTools._unsortedSampleIntegerArray);
+        _testTools.compare(_testTools._unsortedSampleIntegerArray, _testTools._sortedSampleIntegerArray);
+    }
 }
