576b8ff8546d0b22ea17fcfd95df6aaedbf238ae
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-16 10:41:40 -0800

    Added boilerplate code to the ShellSort test file.

--- src/Sort/ComparisonSort/ShellSort/ShellSort_Test.java
+++ src/Sort/ComparisonSort/ShellSort/ShellSort_Test.java
@@ -1,6 +1,11 @@
 package Sort.ComparisonSort.ShellSort;
 
+import Sort.AlgorithmTestTools;
 import org.junit.Test;
 
 public class ShellSort_Test {
+
+    private ShellSort _shellSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+
 }
