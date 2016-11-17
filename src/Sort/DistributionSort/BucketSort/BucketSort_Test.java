ddf4d4fc541e1be5768014e8550f5b25f7b07efd
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-17 10:07:17 -0800

    Added a unit test for the bucketSort() method within the BucketSort class; currently troubleshooting bugs within the bucketSort() method.

--- src/Sort/DistributionSort/BucketSort/BucketSort_Test.java
+++ src/Sort/DistributionSort/BucketSort/BucketSort_Test.java
@@ -6,4 +6,11 @@ import org.junit.Test;
 public class BucketSort_Test {
     private BucketSort _bucketSort;
     private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+
+    @Test
+    public void testBucketSort() throws Exception{
+        _bucketSort = new BucketSort();
+        _bucketSort.bucketSort(_testTools._unsortedSampleIntegerArray);
+        _testTools.compare(_testTools._unsortedSampleIntegerArray, _testTools._sortedSampleIntegerArray);
+    }
 }
