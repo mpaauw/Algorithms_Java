85c94468d87ea2ad9344f3fa06ab5b48f0015c6d
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-17 10:05:27 -0800

    Extended boilerplate code within the BucketSort test file.

--- src/Sort/DistributionSort/BucketSort/BucketSort_Test.java
+++ src/Sort/DistributionSort/BucketSort/BucketSort_Test.java
@@ -1,4 +1,9 @@
 package Sort.DistributionSort.BucketSort;
 
+import Sort.AlgorithmTestTools;
+import org.junit.Test;
+
 public class BucketSort_Test {
+    private BucketSort _bucketSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
 }
