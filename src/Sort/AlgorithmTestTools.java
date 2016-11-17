cee5dcc3a43cb398b4975e010c11a9c9a3f93b1b
Matt Paauw <matthew.e.paauw@boeing.com>
2016-11-17 09:56:30 -0800

    Added an algorithm to the bucketSort() method wtihin the BucketSort class.

--- src/Sort/AlgorithmTestTools.java
+++ src/Sort/AlgorithmTestTools.java
@@ -9,7 +9,7 @@ public class AlgorithmTestTools {
     public int[] _unsortedSampleIntegerArray;
     public int[] _sortedSampleIntegerArray;
     private int _sampleIntegerArraySize;
-    private final int UPPER_BOUND = 200000;
+    private final int UPPER_BOUND = 20;
 
     public AlgorithmTestTools(){
         int size = _randomizer.nextInt(UPPER_BOUND) + 1;
--- src/Sort/DistributionSort/BucketSort.java
+++ src/Sort/DistributionSort/BucketSort.java
@@ -1,4 +1,52 @@
 package Sort.DistributionSort;
 
+import Sort.SimpleSort.InsertionSort.InsertionSort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BucketSort {
+
+    InsertionSort _insertionSort = new InsertionSort();
+
+    /*
+    * SUMMARY:  Takes an unsorted array and performs a Bucket Sort on it.
+    *           Efficiency: O(n + k)
+    * */
+    public void bucketSort(int[] inputArray){
+        int max = 0, min = 0;
+        getMaxMin(max, min, inputArray);
+        int numBuckets = (int)Math.sqrt(inputArray.length);
+        ArrayList<Integer>[] buckets = new ArrayList[numBuckets];
+        int divisor = (int)Math.ceil((max + 1) / numBuckets);
+        for(Integer item : inputArray){
+            int j = (int)Math.floor(item / divisor);
+            buckets[j].add(item);
+        }
+
+        for(int i = 0; i < buckets.length; i++){
+            int[] bucketArray = Arrays.stream(buckets[i].toArray(new Integer[buckets[i].size()])).mapToInt(Integer::intValue).toArray();
+            _insertionSort.insertionSort(bucketArray);
+        }
+
+        int reinsertIter = 0;
+        for(int i = 0; i < buckets.length; i++){
+            for(Integer sortedItem : buckets[i]){
+                inputArray[reinsertIter] = sortedItem;
+                reinsertIter++;
+            }
+        }
+    }
+
+    private static void getMaxMin(int max, int min, int[] inputArray){
+        for(Integer item : inputArray){
+            if(item > max){
+                max = item;
+            }
+            if(item <= min){
+                min = item;
+            }
+        }
+    }
+
 }
