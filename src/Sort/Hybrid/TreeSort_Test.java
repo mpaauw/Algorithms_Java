0534aec85c8eaa2b5616ea99eeb44183b18ef38f
adjutant-xx <matt.paauw@gmail.com>
2017-01-10 09:36:12 -0800

    Finished writing TreeSort_Test unit test.

--- src/Sort/Hybrid/TreeSort_Test.java
+++ src/Sort/Hybrid/TreeSort_Test.java
@@ -1,4 +1,16 @@
 package Sort.Hybrid;
 
+import Sort.AlgorithmTestTools;
+import org.junit.Test;
+
 public class TreeSort_Test {
+    private TreeSort _treeSort;
+    private AlgorithmTestTools _testTools = new AlgorithmTestTools();
+
+    @Test
+    public void testTreeSort() throws Exception{
+        _treeSort = new TreeSort();
+        _treeSort.treeSort(_testTools._unsortedSampleIntegerArray);
+        _testTools.compare(_testTools._unsortedSampleIntegerArray, _testTools._sortedSampleIntegerArray);
+    }
 }
