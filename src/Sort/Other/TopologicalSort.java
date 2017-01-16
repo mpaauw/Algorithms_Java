1e5751f32765334bfc9a2c1b6a338caba044c618
adjutant-xx <matt.paauw@gmail.com>
2017-01-16 11:38:22 -0800

    Continued work on topological sort algorithm; added a new 'Resources' package to the same directory as the 'Sort' package, to be used for including shared classes.

--- src/Sort/Other/TopologicalSort.java
+++ src/Sort/Other/TopologicalSort.java
@@ -1,4 +1,13 @@
 package Sort.Other;
 
+import java.util.Stack;
+
 public class TopologicalSort {
+
+    private Stack<Integer> _stack = new Stack();
+
+    public void topologicalSort(int[] inputArray){
+
+    }
+
 }
